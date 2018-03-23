47d66e2ea80d8947e8c88322bbbba3d0937c9f7a
Louis ULMANN <louis.ulmann@crossquantum.com>
2018-03-23 15:03:11 +0100

    Component lifecycle

--- src/app.js
+++ src/app.js
@@ -9,6 +9,27 @@ class IndecisionApp extends React.Component {
       options: props.options
     }
   }
+  componentDidMount() {
+    try {
+      const json = localStorage.getItem('options');
+      const options = JSON.parse(json);
+
+      if (options) {
+        this.setState(() => ({ options }));
+      }
+    } catch (e) {
+      // NOPE
+    }
+  }
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.options.length !== this.state.options.length) {
+      const json = JSON.stringify(this.state.options);
+      localStorage.setItem('options', json);
+    }
+  }
+  componentWillunMount() {
+    console.log('Hello I will Unmount');
+  }
   handleDeleteOptions() {
     this.setState(() => ({ options: [] }));
   }
@@ -87,6 +108,7 @@ const Options = (props) => {
   return (
     <div>
       <button onClick={props.handleDeleteOptions}>Remove all</button>
+      {props.options.length === 0 && <p>Please add an option to get started</p>}
       {
         props.options.map((option) => (
           <Option
@@ -129,6 +151,10 @@ class AddOption extends React.Component {
     const error = this.props.handleAddOption(option);
 
     this.setState(() => ({ error }))
+
+    if (!error) {
+      e.target.elements.option.value = ''; 
+    }
   }
   render () {
     return (
